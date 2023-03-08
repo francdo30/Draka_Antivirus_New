e4e73c6015e445907a40704e40db130474bd2a90
francdo30 <104600818+francdo30@users.noreply.github.com>
2023-03-08 14:45:56 +0100

    san_automatique
    détection automatique de la USB et scan global

---
+++ Database.cs
@@ -0,0 +1,420 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScanAutomatique
+{
+    internal class Database
+    {// chemin jusqu'au repertoire courant
+        public static string targetPath = AppDomain.CurrentDomain.BaseDirectory;
+        public static string name_db = "WanDataBase.db";
+        public static string sourceFile = targetPath + name_db;
+        string path = targetPath + "Error_Log.txt";
+
+        //string path = @"C:\Draka_Antivirus\Draka_Antivirus\bin\Debug\Error_Log.txt";
+        /*string path = @"C:\Program Files (x86)\Default Company Name\Setup1\Error_Log.txt";*/
+
+        public void ErrorMessage(string sql, string e)
+        {
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+                TextWriter tw = new StreamWriter(path, true);
+                tw.WriteLine(DateTime.Now.ToString() + " " + "Request :" + " " + sql + " " + "Error_Message :" + e);
+                tw.Close();
+            }
+
+            else if (File.Exists(path))
+            {
+                TextWriter tw = new StreamWriter(path, true);
+                tw.WriteLine(DateTime.Now.ToString() + " " + "Request:" + " " + sql + " " + "Error_Message:" + e);
+                tw.Close();
+            }
+        }
+
+
+        /* 
+         * Création d'une base de données SQLite 
+         */
+        public string createDatabase(string name_db)
+        {
+            string sourceFile = targetPath + name_db;
+
+            if (File.Exists(sourceFile) != true)
+            {
+                //MessageBox.Show("creation de la base de données");
+                try
+                {
+                    File.Create(sourceFile);
+                    AutoClosingMessageBox.Show("BD créer = " + sourceFile);
+                }
+                catch(Exception ex) 
+                {
+                    MessageBox.Show("Internal Error : " + ex.Message);
+                    Console.WriteLine("Internal Error : " + ex.Message);
+                    ErrorMessage("Create Database ", ex.ToString());
+                    return null;
+                }
+            }
+                return sourceFile;
+        }
+
+
+        /* 
+         * Supprimer une base de données SQLite 
+         */
+        public Boolean deleteDatabase(string name_db)
+        {
+            string sourceFile = targetPath + name_db;
+
+            if (File.Exists(sourceFile) == true)
+            {
+                try
+                {
+                    File.Delete(sourceFile);
+                }
+                catch (Exception ex)
+                {
+                    /*MessageBox.Show("Internal Error : " + ex.Message);
+                    Console.WriteLine("Internal Error : " + ex.Message);*/
+                    ErrorMessage("Delete Database ", ex.ToString());  // truncate table
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /* 
+         * Inserer des données dans une base de données SQLite
+         */
+        public Boolean insertData(string sourceFile, string sql)
+        {
+            if (File.Exists(sourceFile) == true)
+            {
+                try
+                {
+                    var con = new SQLiteConnection("Data Source=" + sourceFile + ";");
+                    var cmd = new SQLiteCommand(sql, con);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    //AutoClosingMessageBox.Show("Insertion réussi");
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Internal Error : " + ex.Message);
+                    Console.WriteLine("Internal Error : " + ex.Message);
+                    ErrorMessage(sql, ex.ToString());
+                    return false;
+                }
+            }
+            MessageBox.Show("Insertion réussi");
+            return true;
+        }
+
+        // creation d'une table de la base de donnée
+        public Boolean CreateTable(string sourceFile, string nameTable)
+        {
+            if (!File.Exists(sourceFile))
+            {
+                sourceFile = createDatabase(name_db);
+                MessageBox.Show("Base de données bien creeeeeeeeeeeeeeeeer");
+            }
+            else
+            {               
+                String sql = "CREATE TABLE " + nameTable + "(Id Integer primary key autoincrement, date text, taille text, directory text, statut text)" + ";";
+                string myCommand = "CREATE TABLE " + nameTable + "(url varchar(128) not null primary key, status varchar(128), date varchar(128))";
+
+                if (nameTable == "Quarantaine")
+                {
+                    MessageBox.Show("je suis la table quarantaine ");
+                    try
+                    {
+                        MessageBox.Show("ouffffffffffffff ");
+                        var con = new SQLiteConnection("Data Source=" + sourceFile + ";");
+                        MessageBox.Show("tttttttttttt ");
+                        var cmd = new SQLiteCommand(sql, con);
+                        con.Open();
+                        MessageBox.Show("Table creer etape1");
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Table creer etape2");
+                        con.Close();
+                        AutoClosingMessageBox.Show("la table quantaine a été créer avec succes");
+                        return true;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Internal Error : " + ex.Message);
+                        //Console.WriteLine("Internal Error : " + ex.Message);
+                        ErrorMessage(sql, ex.ToString());
+                        return false;
+                    }
+                }
+                else if (nameTable == "parentControl")
+                {
+                    try
+                    {
+                        var con = new SQLiteConnection("Data Source=" + sourceFile + ";");
+                        var cmd = new SQLiteCommand(myCommand, con);
+                        con.Open();
+                        //MessageBox.Show("Table creer etape1");
+                        cmd.ExecuteNonQuery();
+                        //MessageBox.Show("Table creer etape2");
+                        con.Close();
+                        //AutoClosingMessageBox.Show("la table parentControl a été créer avec succes");
+                        return true;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Internal Error : " + ex.Message);
+                        //Console.WriteLine("Internal Error : " + ex.Message);
+                        ErrorMessage(sql, ex.ToString());
+                        return false;
+                    }
+                }
+                else if (nameTable == "FullScan")
+                {
+                    string myCommand1 = "CREATE TABLE " + nameTable + "(Id INTEGER AUTOINCREMENT not null primary key ,date text , taille text, statut varchar(128))";
+
+                    try
+                    {
+                        var con = new SQLiteConnection("Data Source=" + sourceFile + ";");
+                        var cmd = new SQLiteCommand(myCommand1, con);
+                        con.Open();
+                        //MessageBox.Show("Table creer etape1");
+                        cmd.ExecuteNonQuery();
+                        //MessageBox.Show("Table creer etape2");
+                        con.Close();
+                        //  AutoClosingMessageBox.Show("la table FullScan a été créer avec succes");
+                        return true;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Internal Error : " + ex.Message);
+                        //Console.WriteLine("Internal Error : " + ex.Message);
+                        ErrorMessage(sql, ex.ToString());
+                        return false;
+                    }
+                }
+            }
+            //MessageBox.Show("Table creer avec succes");
+            return true;
+        }
+        /* 
+         * Sélectionner des données dans une base de données SQLite
+         */
+        public List<Object[]> selectDatas(string sourceFile, string sql)
+        {
+            List<Object[]> liste = null;
+
+            if (File.Exists(sourceFile) == true)
+            {
+                try
+                {
+                    var con = new SQLiteConnection("Data Source=" + sourceFile + ";");
+                    var cmd = new SQLiteCommand(sql, con);
+
+                    con.Open();
+                    SQLiteDataReader datas = cmd.ExecuteReader();
+                    if (datas.HasRows == true)
+                    {
+                        liste = new List<Object[]>();
+                        while (datas.Read())
+                        {
+                            Object[] element = new Object[datas.FieldCount];
+                            for (var i = 0; i < datas.FieldCount; i++)
+                            {
+                                element[i] = datas.GetValue(i);
+                            }
+
+                            liste.Add(element);
+                        }
+                    }
+                    con.Close();
+                }
+                catch (Exception ex)
+                {
+                    /*MessageBox.Show("Internal Error : " + ex.Message);*//*
+                    Console.WriteLine("Internal Error : " + ex.Message);*/
+                    ErrorMessage(sql, ex.ToString());
+                }
+            }
+
+            return liste;
+        }
+
+
+
+
+        /* 
+         * Sélectionner des données dans une base de données SQLite
+         * Et ajouter une donnée pour incrémenter automatiquement
+         * le numéro ou l'identifiant de la table
+         */
+        public List<Object[]> selectDatasAuto(string sourceFile, string sql)
+        {
+            List<Object[]> liste = null;
+
+            if (File.Exists(sourceFile) == true)
+            {
+                try
+                {
+                    var con = new SQLiteConnection("Data Source=" + sourceFile + ";");
+                    var cmd = new SQLiteCommand(sql, con);
+
+                    con.Open();
+                    SQLiteDataReader datas = cmd.ExecuteReader();
+                    if (datas.HasRows == true)
+                    {
+                        int j = 1;
+                        liste = new List<Object[]>();
+                        while (datas.Read())
+                        {
+                            Object[] element = new Object[datas.FieldCount + 1];
+
+                            for (var i = 0; i < (datas.FieldCount + 1); i++)
+                            {
+                                if (i == 0)
+                                {
+                                    element[i] = j;
+                                }
+                                else
+                                {
+                                    element[i] = datas.GetValue(i - 1);
+                                }
+                            }
+
+                            liste.Add(element);
+                            j = j + 1;
+                        }
+                    }
+                    con.Close();
+                }
+                catch (Exception ex)
+                {
+                    /*MessageBox.Show("Internal Error : " + ex.Message);*/
+                    /*Console.WriteLine("Internal Error : " + ex.Message);*/
+                    ErrorMessage(sql, ex.ToString());
+                }
+            }
+
+            return liste;
+        }
+
+        /* 
+         * Sélectionner une donnée dans une base de données SQLite
+         */
+        public Object[] searchData(string sourceFile, string sql)
+        {
+            Object[] element = null;
+
+            if (File.Exists(sourceFile) == true)
+            {
+                MessageBox.Show("eeeeeee");
+                try
+                {
+                    var con = new SQLiteConnection("Data Source  = " + sourceFile + ";");
+                    var cmd = new SQLiteCommand(sql, con);
+
+                    con.Open();
+                    SQLiteDataReader datas = cmd.ExecuteReader();
+                    //MessageBox.Show("eeeeeeetape1");
+                    if (datas.HasRows == true)
+                    {
+                        //MessageBox.Show("Eeetape2");
+                        while (datas.Read())
+                        {
+                            element = new Object[datas.FieldCount];
+                            for (var i = 0; i < datas.FieldCount; i++)
+                            {
+                                element[i] = datas.GetValue(i);
+                            }
+                        }
+                    }
+                    con.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Internal Error : " + ex.Message);
+                    Console.WriteLine("Internal Error : " + ex.Message);
+                    ErrorMessage(sql, ex.ToString());
+                }
+            }
+
+            return element;
+        }
+
+        // suprimer les données d'une table
+
+        public Boolean ClearTable(string name_db)
+        {
+            string sourceFile = targetPath + name_db;
+            string sql = "DELETE Quarantaine WHERE;";
+            string sql1 = "VACUUM Quarantaine ;";
+
+            if (File.Exists(sourceFile) == true)
+            {
+                MessageBox.Show("je suis dans le cleanTable");
+                try
+                {
+                    var con = new SQLiteConnection("Data Source=" + sourceFile + ";");
+                    var cmd = new SQLiteCommand(sql, con);
+                    var cmd1 = new SQLiteCommand(sql1, con);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    cmd1.ExecuteNonQuery();
+                    con.Close();
+                    MessageBox.Show("Table Quarantaine éffacé avec succes");
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    //AutoClosingMessageBox.Show("Internal Error : " + ex.Message);
+                    Console.WriteLine("Internal Error : " + ex.Message);
+                    ErrorMessage(sql, ex.ToString());
+                    return false;
+                }
+
+            }
+            return true;
+        }
+
+
+        /* 
+         * Supprimer des données dans une base de données SQLite
+         */
+        public Boolean deleteData(string sourceFile, string sql)
+        {
+            if (File.Exists(sourceFile) == true)
+            {
+                try
+                {
+                    var con = new SQLiteConnection("Data Source=" + sourceFile + ";");
+                    var cmd = new SQLiteCommand(sql, con);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show(" Fichier supprimer de la BD ");
+                    con.Close();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    /* MessageBox.Show("Internal Error : " + ex.Message);
+                     Console.WriteLine("Internal Error : " + ex.Message);*/
+                    ErrorMessage(sql, ex.ToString());
+                    return false;
+                }
+            }
+
+            return false;
+        }
+
+
+    }
+}
---
+++ UsbInterface.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScanAutomatique
+{
+    public partial class UsbInterface : Form
+    {
+        private System.Threading.ManualResetEvent _busy = new System.Threading.ManualResetEvent(false);
+        public static string targetPath = AppDomain.CurrentDomain.BaseDirectory;
+        public static string name_db = "ScanDataBase.db";
+        public static string path = "";
+        public static string sourceFile = targetPath + "MD5Base.txt";
+        string db1 = "rr";
+        Color[] colors = { Color.Aqua, Color.Green, Color.Blue, Color.Black, Color.DeepSkyBlue, Color.Red };
+
+        // nous sommes dans la classe Scan qui sera auto appelé dans le constructeur        
+
+        private const int WM_DEVICECHANGE = 0x219;
+        private const int DBT_DEVICEARRIVAL = 0x8000;
+        private const int DBT_DEVICEREMOVECOMPLETE = 0x8004;
+        private const int DBT_DEVTYP_VOLUME = 0x00000002;
+        //private bool isVisibleCore = false;
+        public UsbInterface()
+        {
+            InitializeComponent();
+            MessageBox.Show("Click sur okey pour déclencher le scan automatique ");
+            ScanTotal scanT = new ScanTotal();
+            
+            //SetVisibleCore(isVisibleCore);
+            /*this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(Screen.PrimaryScreen.WorkingArea.Width - this.Width,
+                                   Screen.PrimaryScreen.WorkingArea.Height - this.Height);*/
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(Screen.PrimaryScreen.WorkingArea.Width ,
+                                   Screen.PrimaryScreen.WorkingArea.Height );
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            switch (m.Msg)
+            {
+                case WM_DEVICECHANGE:
+                    switch ((int)m.WParam)
+                    {
+                        case DBT_DEVICEARRIVAL:
+                            //listBox1.Items.Add("New Device Arrived");
+                            //MessageBox.Show("Clé branché, En attente du lancement du Scan Auto");
+                            int devType = Marshal.ReadInt32(m.LParam, 4);
+                            if (devType == DBT_DEVTYP_VOLUME)
+                            {
+                                DevBroadcastVolume vol;
+                                vol = (DevBroadcastVolume)Marshal.PtrToStructure(m.LParam, typeof(DevBroadcastVolume));
+                                /*listBox1.Items.Add("Mask is " + vol.Mask);
+                                listBox1.Items.Add("Letter is " + GetLetter(vol.Mask));*/
+                                path = GetLetter(vol.Mask).ToString() + @":\";
+                                //MessageBox.Show("Chemin : " + path);
+                                //File.Copy(sourceFile, path);
+                                CheckRemove(path);
+                            }
+                            break;
+
+                        case DBT_DEVICEREMOVECOMPLETE:
+                            //listBox1.Items.Add("Device Removed");
+                            MessageBox.Show("Clé débranché");
+                            //CheckRemove(path);
+                            break;
+                    }
+                    break;
+            }
+        }
+
+        public void CheckRemove(string str)
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo d in allDrives)
+            {
+                if (d.IsReady == true)
+                {
+                    double free, total;
+                    free = d.TotalFreeSpace / Math.Pow(1024, 3);
+                    total = d.TotalSize / Math.Pow(1024, 3);
+                    //MessageBox.Show("ll : "+d);
+                    string usb = d.Name;
+                    
+                    //MessageBox.Show("Total Free : " + free + " Total size : " + total);
+                    if (usb.Equals(str))
+                    {
+                        Form1 DetectionUSB = new Form1(str);
+                        DetectionUSB.StartPosition = FormStartPosition.Manual;
+                        DetectionUSB.Location = new Point(Screen.PrimaryScreen.WorkingArea.Width - DetectionUSB.Width,
+                                               Screen.PrimaryScreen.WorkingArea.Height - DetectionUSB.Height);
+                        //DirectoryInfo lecteurUSB = new DirectoryInfo(d.RootDirectory.ToString());
+                        //MessageBox.Show("Je suis la clé usb branché : "+ lecteurUSB.FullName);
+                        //int Taille = (int)(total - free);
+                        //MessageBox.Show("gggg : " + Taille);
+
+                        // ici, on appel la méthode scan complet sur la clé connecté                        
+                        DetectionUSB.Show();
+                    }
+                }
+
+
+            }
+            /*string tt = @"\Program Files\" + str;
+            MessageBox.Show(tt);*/
+        }
+
+        // les sous méthodes
+
+        public char GetLetter(int mask)
+        {
+            int ch = 0;
+            for (; ch < 26; ch++)
+            {
+                if ((mask & 0x1) == 0x1)
+                    break;
+                mask >>= 1;
+            }
+            ch += 0x41;
+            return (char)ch;
+        }
+
+        private void UsbInterface_Resize(object sender, System.EventArgs e)
+        {
+            if (FormWindowState.Minimized == WindowState)
+                Hide();
+        }
+
+        private void UsbInterface_Load(object sender, EventArgs e)
+        {
+           /* this.Visible = false;
+            this.WindowState = FormWindowState.Normal;*/
+           this.Hide(); 
+        }
+    }
+}
